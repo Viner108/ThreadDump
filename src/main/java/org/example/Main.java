4209908829c299726fafe2da6c1cf2f9d8a54bd0
Viner108 <andrukovichl@mail.ru>
2024-02-02 12:20:28 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start");
+            Thread.sleep(50000);
+        System.out.println("Finished");
+    }
+}
