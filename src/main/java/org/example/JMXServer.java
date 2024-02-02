0786bfb8e04fb5cf4dfedb7db2172872921b75f9
Viner108 <andrukovichl@mail.ru>
2024-02-02 17:03:17 +0300

    server commit

--- src/main/java/org/example/JMXServer.java
+++ src/main/java/org/example/JMXServer.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.management.Attribute;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
@@ -7,8 +8,12 @@ import java.lang.management.ManagementFactory;
 public class JMXServer {
     private static HelloWorld helloWorld;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         MBeanServer mBeanServer= ManagementFactory.getPlatformMBeanServer();
         ObjectName objectName=new ObjectName("org.example:type=HelloWorld");
+        helloWorld =new HelloWorld();
+        mBeanServer.registerMBean(helloWorld,objectName);
+        System.out.println("JMX server is runnig...");
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
